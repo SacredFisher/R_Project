d19f24699c24414f79f1bcfb8e08c84ca24a64bf
SacredFisher <srikar.pamidi@gmail.com>
2022-02-10 18:32:11 -0600

    Changed to Music Databases

--- project.R
+++ project.R
@@ -1,7 +1,25 @@
 library(tidyverse)
-orbits <- read_csv('orbits.csv')
-impacts <- read_csv('impacts.csv')
+tank <- read_csv('Shark Tank Companies.csv')
 
+summary(tank)
+str(tank)
+cor(tank$deal,tank$askedFor)
+cor(tank$deal,tank$exchangeForStake)
+cor(tank$deal, tank$valuation)
+cor(tank$askedFor, tank$valuation)
 
-full_join(orbits, impacts, by='Object Name')
+tank[c('askedFor','exchangeForStake','valuation')] %>% cor()
 
+tank %>% ggplot(aes(askedFor, valuation)) + geom_point() 
+
+tank %>% ggplot() +geom_bar(mapping = aes(x = category), tank$deal)
+
+###Tidy up the data so that the categories each have their own stuff in them
+##furthemore try analyzing corre
+tank %>% filter(deal) %>% select(c(category,deal)) -> modtank
+count(modtank,category) -> countn
+keep(countn$category, countn$n > 5)->categories
+
+modtank[categories]
+
+ggplot() + geom_bar(mapping = aes(x=category))
